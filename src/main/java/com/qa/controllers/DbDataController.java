abe4d186240745184397b31390b90e77c4825aca
Karolina <Karolina.Dudek@academytrainee.com>
2019-09-18 20:52:04 +0100

    added update function

--- src/main/java/com/qa/controllers/DbDataController.java
+++ src/main/java/com/qa/controllers/DbDataController.java
@@ -2,6 +2,7 @@ package com.qa.controllers;
 import com.qa.models.DbData;
 import com.qa.repository.DbDataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -28,6 +29,18 @@ public class DbDataController {
     public DbData deleteDice(@PathVariable Long id){
         DbData existing = repository.findOne(id);
         repository.delete(existing);
+        return existing;
+    }
+
+    @RequestMapping(value = "dice/{id}", method = RequestMethod.PUT)
+    public DbData updateDice(@PathVariable Long id, @RequestBody DbData dice){
+        DbData existing = repository.findOne(id);
+
+        existing.setAmount(dice.getAmount());
+        existing.setColour(dice.getColour());
+        existing.setMaterial(dice.getMaterial());
+        existing.setType(dice.getType());
+
         return existing;
     }
 }
